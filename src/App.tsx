74f5e51e2aae894632e16262e4b7d4e58c8f5c5f
sonas <fatoumata-sona.sylla@etudiant.univ-rennes1.fr>
2024-05-21 11:11:06 +0200

    initialiser un état avec hook d'effet useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { FunctionComponent, useState } from "react";
+// les imports
+import React, { FunctionComponent, useState, useEffect } from "react";
 import Pokemon from "./models/pokemon";
 import POKEMONS from "./models/mock-pokemon";
 
@@ -13,14 +14,24 @@ import POKEMONS from "./models/mock-pokemon";
 //return <h1>Bonjour, {name} !</h1>;
 //};
 
+//const App: FunctionComponent = () => {
+//const [pokemons] = useState<Pokemon[]>(POKEMONS);
+//return (
+//<div>
+//<h1>Pokédex</h1>
+//<p>Il y a {pokemons.length} pokémons dans le Pokédex.</p>
+// </div>
+// );
+//};
+
 const App: FunctionComponent = () => {
   //const name: String = "React";
-  const [pokemons] = useState<Pokemon[]>(POKEMONS);
-  // code détaillé
-  // var nameStateVariable = useState('React');
-  //var name = nameStateVariable[0];
-  //var setName = nameStateVariable[1];
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
+  // Le hook d'effet
+  useEffect(() => {
+    setPokemons(POKEMONS);
+  }, []);
   return (
     <div>
       <h1>Pokédex</h1>
