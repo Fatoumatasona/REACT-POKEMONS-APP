8a86c3097f5f34cba5931611f4a6ccc51bd82c47
sonas <fatoumata-sona.sylla@etudiant.univ-rennes1.fr>
2024-05-21 17:01:29 +0200

    ajout de la gestion de 2 évenements onMouseEnter et onMouseLeave

--- src/pages/pokemon-list.tsx
+++ src/pages/pokemon-list.tsx
@@ -16,11 +16,7 @@ const PokemonList: FunctionComponent = () => {
       <div className='container'>
         <div className='row'>
           {pokemons.map((pokemon) => (
-            <PokemonCard
-              key={pokemon.id}
-              pokemon={pokemon}
-              borderColor='purple'
-            />
+            <PokemonCard key={pokemon.id} pokemon={pokemon} />
           ))}
         </div>
       </div>
