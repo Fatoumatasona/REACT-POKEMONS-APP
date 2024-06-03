71d6efa9096e83146dc07643fa0e65f4858c0b21
sonas <fatoumata-sona.sylla@etudiant.univ-rennes1.fr>
2024-06-03 17:08:44 +0200

    Utilisation du hook UseHistory pour afficher le détail d'un pokémon

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -3,6 +3,7 @@ import Pokemon from "../models/pokemon";
 import "./pokemon-card.css";
 import formatDate from "../helpers/format-date";
 import formatType from "../helpers/format-type";
+import { useHistory } from "react-router-dom";
 
 type Props = {
   pokemon: Pokemon;
@@ -15,6 +16,7 @@ const PokemonCard: FunctionComponent<Props> = ({
 }) => {
   const [color, setColor] = useState<string>();
 
+  const history = useHistory();
   const showBorder = () => {
     setColor(borderColor);
   };
@@ -23,11 +25,16 @@ const PokemonCard: FunctionComponent<Props> = ({
     setColor("#f5f5f5"); // on remet la bordure en gris.
   };
 
+  const goToPokemon = (id: number) => {
+    history.push(`/pokemons/${id}`);
+  };
+
   // le return permet d'afficher l'ensemble des propriétés
   // modifiées dans le DOM virtuel
   return (
     <div
       className='col s6 m4'
+      onClick={() => goToPokemon(pokemon.id)}
       onMouseEnter={showBorder}
       onMouseLeave={hideBorder}
     >
